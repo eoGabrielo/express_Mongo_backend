dd91908f55e4d6e36faad0dc0e3988a6c0dbe86f
Your Name <you@example.com>
2025-06-19 15:19:39 -0300

    http change

--- src/app.js
+++ src/app.js
@@ -1,31 +1,32 @@
-const express = require('express'); //Import Biblioteca para criar servidores web, rotas e respostas HTTP
-const connectDB = require('./config/database'); //Import arquivo com conecxão ao banco de dados  MongoDB
+const express = require('express'); //Import Biblioteca para criar servidor e rotas HTTP.
+const connectDB = require('./config/database'); //Import arquivo com conecxão ao banco de dados MongoDB.
 const Cliente = require('./models/Cliente'); //Modelo de interação com o banco de dados, usaremos "Cliente..." pro CRUD.
 
 
-const app =  express(); // Variavel que recebe funçoes da biblioteca para criar servidor e suas rotas
+const app =  express(); // Variavel que recebe funçoes da biblioteca para criar servidor e rotas HTTP.
 
-connectDB() //Conectar ao banco
+connectDB() //Conectar ao banco.
 
-//Rota GET quando for acessado '/' na web para mostrar essa resposta.
+//Rota teste GET quando for acessado '/' na web.
 app.get('/', (req, res) => {
-    res.send('Ta rodando, GABRIEL'); //Enviar texto
+    res.send('Ta rodando, GABRIEL');
 });
 
 const PORT = 3000; //Porta do servidor
 
-app.use(express.json()) // Garantir que o servidor consiga ler JSON
+app.use(express.json())//Garantir que o servidor consiga ler JSON.
 
 app.post('/clientes', async(req, res) =>{
     try{
-        //Pegando valores enviado pelo cliente via post se o cliente não enviar nada, req.body vai ser vazio ou undefined, e aí não vai ter valores.
+        //req.body: onde ficam os dados enviados pelo usuario, normalmente no formato JSON
+        //Pegando valores enviado pelo cliente via post, se o cliente não enviar nada, req.body vai ser vazio ou undefined.
         const nome = req.body.nome;
         const email = req.body.email;
         const telefone = req.body.telefone;
         const dataNascimento = req.body.dataNascimento;
         const ativo = req.body.ativo;
 
-        const novoCliente = new Cliente({//Objeto
+        const novoCliente = new Cliente({//Objeto que recebe valores do req.body.
             nome: nome,
             email: email,
             telefone: telefone,
@@ -33,52 +34,55 @@ app.post('/clientes', async(req, res) =>{
             ativo: ativo
 });
 
-        const clienteSalvo = await novoCliente.save(); // salvando esse novo cliente no banco MongoDB.
+        const clienteSalvo = await novoCliente.save(); //Enviando objeto com dados do cliente salvos para o MongoDB.
 
-        res.json(clienteSalvo);//respondendo para o cliente com os dados salvos, envia um objeto JSON como resposta
+        res.json(clienteSalvo);//retorna um JSON para o servidor, para verificar se os dados foram corretos.
     }catch (error){
-            res.send(error.message)//captura erros que possam ocorrer durante a criação ou o salvamento.
+            res.send(error.message)//captura e retornar o erro que pode acontecer ao enviar para o banco de dados.
         };
     }
 );
 
+//Rota GET para retornar clientes cadastrados
 app.get('/clientes', async (req, res)=>{
     try{
-        const clientes =  await Cliente.find();// Busca todos os documentos no banco de dados
-        res.json(clientes); // Envia o array de clientes em JSON
+        const clientes =  await Cliente.find();// Busca todos os documentos no banco de dados.
+        res.json(clientes);//Retornar array dos clientes em JSON.
     }catch(error){
-        res.json({
+        res.json({//Retorna erro se a busca falhar.
             erro: 'Erro ao buscar clientes',
             detalhes: error.message
         });
     }
 });
 
+//Rota para buscar cliente especifico pela chave id criada automatica pelo banco de dados MongoDB.
 app.get('/clientes/:id', async (req, res) => {
   try {
     const id = req.params.id; // Pega o ID da URL
-    const cliente = await Cliente.findById(id); // Busca no banco pelo ID
+    const cliente = await Cliente.findById(id); // Busca no banco de dados pelo ID.
 
-    if (!cliente) {
-      return res.json({ erro: 'Cliente não encontrado' }); // Se não achar
-    }else
-        res.json(cliente); // Retorna o cliente em JSON
-  } catch (error) {
-    res.status(500).json({ erro: 'Erro ao buscar cliente', detalhes: error.message }); // Erro geral
+    if (!cliente) {//Se o valor for falso, retornar um erro em JSON.
+      return res.json({ erro: 'Cliente não encontrado' });
+    }else//Se existir valor, retornar o cliente em JSON.
+        res.json(cliente); 
+  } catch (error) {//Se a resposta ao banco de dados der erro.
+    res.json({ erro: 'Erro ao buscar cliente', detalhes: error.message });
   }
 });
 
+//Rota para excluir cliente pelo id.
 app.delete('/clientes/:id', async (req, res) => {
   try {
-    const id = req.params.id; // Pega o ID da URL
-    const cliente = await Cliente.findByIdAndDelete(id); // Tenta deletar no banco
+    const id = req.params.id; // Pega o id da URL.
+    const cliente = await Cliente.findByIdAndDelete(id); // Tenta deletar no banco.
 
-    if (!cliente) {
-      return res.json({ erro: 'Cliente não encontrado' }); // Se não achar, retorna erro
-    }else
-        res.json({ mensagem: 'Cliente deletado com sucesso' }); // Confirma remoção
+    if (!cliente) {// Se não achar o cliente pelo id, retorna erro.
+      return res.json({ erro: 'Cliente não encontrado' });
+    }else//Se achar o cliente, confirma remoção.
+        res.json({ mensagem: 'Cliente deletado com sucesso' });
   } catch (error) {
-    res.status(500).json({ erro: 'Erro ao deletar cliente', detalhes: error.message }); // Erro geral
+    res.json({ erro: 'Erro ao deletar cliente', detalhes: error.message }); // Erro geral.
   }
 });
 
--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,16 @@
-const mongoose =  require('mongoose'); //Import biblioteca banco de dados para o crud
+const mongoose =  require('mongoose'); //Import biblioteca "mongoose" para conexão ao banco de dados
 
-//Func async para conexao ao banco dedados, permite uso do await
+//Func async para conexao ao banco de dados, permite uso do await
 const connectDB = async () => {
-    //try e catch tenta a conexao e captura erros
+    //try e catch para tentar a conexao e captura erros
     try{
-        //Tentativa de conexao com o MongoDB
-        await mongoose.connect('mongodb+srv://gabrielobank:qKs01IAZVw14vvLJ@gabrielo.qymc21h.mongodb.net/', {
+        await mongoose.connect('mongodb+srv://gabrielobank:qKs01IAZVw14vvLJ@gabrielo.qymc21h.mongodb.net/', {//Tentativa de conexao com o MongoDB, link do banco de dados usado do "https://www.mongodb.com/"
             useUniFiedTopology: true, //Garantir compatibilidade e evitar warnings
         });
         console.log('MongoDB conectado com sucesso')
-        //Qualquer erro de conexao, cai no catch
-    }catch (error) {
-        console.error('Error ao conectar com MongoDB', error);
-        process.exit(1); //Trava o sistema caso o banco de dados falhar!!
+    }catch (error) {//Qualquer erro de conexao, cai no catch
+        console.error('Error ao conectar com MongoDB', error); //Loga erro no console
+        process.exit(1); //Trava o sistema caso o banco de dados falhar!
     }
 }
 
