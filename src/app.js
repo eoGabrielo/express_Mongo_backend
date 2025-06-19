6779f41f3c01468d3c533ac3361c7fb1a80ab079
Your Name <you@example.com>
2025-06-19 11:38:07 -0300

    http delete

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.post('/clientes', async(req, res) =>{
     try{
         const {nome, email, telefone, dataNascimento, ativo} = req.body;
 
-        const novoCliente = new Cliente({
+        const novoCliente = new Cliente({//Objeto 
             nome,
             email,
             telefone,
@@ -29,11 +29,11 @@ app.post('/clientes', async(req, res) =>{
             ativo
         })
 
-        const clienteSalvo = await novoCliente.save();
+        const clienteSalvo = await novoCliente.save(); // salvando esse novo cliente no banco MongoDB.
 
-        res.json(clienteSalvo);
+        res.json(clienteSalvo);//respondendo para o cliente com os dados salvos, envia um objeto JSON como resposta
     }catch (error){
-            res.send(error.message)
+            res.send(error.message)//captura erros que possam ocorrer durante a criação ou o salvamento.
         };
     }
 );
@@ -49,6 +49,36 @@ app.get('/clientes', async (req, res)=>{
         });
     }
 });
+
+app.get('/clientes/:id', async (req, res) => {
+  try {
+    const id = req.params.id; // Pega o ID da URL
+    const cliente = await Cliente.findById(id); // Busca no banco pelo ID
+
+    if (!cliente) {
+      return res.json({ erro: 'Cliente não encontrado' }); // Se não achar
+    }else
+        res.json(cliente); // Retorna o cliente em JSON
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao buscar cliente', detalhes: error.message }); // Erro geral
+  }
+});
+
+app.delete('/clientes/:id', async (req, res) => {
+  try {
+    const id = req.params.id; // Pega o ID da URL
+    const cliente = await Cliente.findByIdAndDelete(id); // Tenta deletar no banco
+
+    if (!cliente) {
+      return res.json({ erro: 'Cliente não encontrado' }); // Se não achar, retorna erro
+    }else
+        res.json({ mensagem: 'Cliente deletado com sucesso' }); // Confirma remoção
+  } catch (error) {
+    res.status(500).json({ erro: 'Erro ao deletar cliente', detalhes: error.message }); // Erro geral
+  }
+});
+
+
 
 //Express inicia o servidor na porta e retorna um callback console.log...
 app.listen(PORT, () => {
