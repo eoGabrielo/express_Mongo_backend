36119c74f08dbfcad7d7391ad86ffe458d3a0813
Gabriel José <105285297+eoGabrielo@users.noreply.github.com>
2025-06-23 05:11:39 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,7 @@
 const mongoose =  require('mongoose'); //Import biblioteca "mongoose" para conexão ao banco de dados
+require('dotenv').config(); // Carrega as variáveis do .env
+
+const MONGO_URI = process.env.MONGODB_URI;//Porta do servidor no arquivo ".env"
 
 //Func async para conexao ao banco de dados, permite uso do await
 const connectDB = async () => {
